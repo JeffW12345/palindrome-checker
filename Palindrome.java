ffc28f747513f4a86d9996582fc4a6f4818e3d70
Jeff Waters <watersjg@gmail.com>
2020-10-17 15:07:13 +0100

    Made improvements in relation to invalid user selections

--- Palindrome.java
+++ Palindrome.java
@@ -2,6 +2,9 @@ import java.util.Scanner;
 
 public class Palindrome {
 
+	private static String userInput;
+	private static boolean validSelectionMade = false;
+
 	private static boolean isPalindrome(String wordOrPhrase) {
 		int length = wordOrPhrase.length();
 		char[] destArray = new char[length];
@@ -26,19 +29,49 @@ public class Palindrome {
 		return true; // If 'false' hasn't been returned, it must be a palindrome.
 	}
 
+	public static void getUserInput() {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter a word or phrase to check: ");
+		while (!validSelectionMade) {
+			String wordOrPhrase = scanner.nextLine();
+			if (wordOrPhrase.length() < 1 && !validSelectionMade) {
+				System.out.println("You did not enter anything. Please try again.");
+				getUserInput();
+			}
+
+			if (!containsLetters(wordOrPhrase) && !validSelectionMade) {
+				System.out.println("You need to enter a word or phrase with at least two letters.");
+				System.out.println("Please try again.");
+				getUserInput();
+			}
+			if (wordOrPhrase.length() >= 1 && containsLetters(wordOrPhrase)) {
+				userInput = wordOrPhrase;
+				validSelectionMade = true;
+			}
+		}
+		scanner.close();
+	}
+
+	// Returns false if < two A-Z and a-z characters, and tre otherwise.
+
+	private static boolean containsLetters(String wordOrPhraseRaw) {
+		String lettersOnlyLowerCase = wordOrPhraseRaw.replaceAll("[^a-zA-Z]", ""); // No non-letter characters
+		if (lettersOnlyLowerCase.length() < 2) {
+			return false;
+		}
+		return true;
+	}
+
 	public static void main(String[] args) {
-		Scanner userInput = new Scanner(System.in);
 		System.out.println("PALINDROME CHECKER");
 		System.out.println("------------------\n");
-		System.out.println("Enter a word or phrase to check: ");
-		String wordOrPhraseRaw = userInput.nextLine();
+		getUserInput();
 		// Word or phrase removed of non-letter characters and made lower case.
-		String lettersOnlyLowerCase = wordOrPhraseRaw.replaceAll("[^a-zA-Z]", "").toLowerCase();
+		String lettersOnlyLowerCase = userInput.replaceAll("[^a-zA-Z]", "").toLowerCase();
 		if (isPalindrome(lettersOnlyLowerCase)) {
-			System.out.println("\'" + wordOrPhraseRaw + "\' is a palindrome");
+			System.out.println("\'" + userInput + "\' is a palindrome");
 		} else {
-			System.out.println("\'" + wordOrPhraseRaw + "\' is NOT a palindrome");
+			System.out.println("\'" + userInput + "\' is NOT a palindrome");
 		}
-		userInput.close();
 	}
 }
